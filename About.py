7856a2dc833a62521828a4a90873fa59a7c82223
Edima <edimaudo@gmail.com>
2025-08-02 15:50:56 +0000

    updated about

--- About.py
+++ About.py
@@ -6,7 +6,7 @@ st.header(ABOUT_HEADER)
 st.markdown(
     """
     Focusing on the theme - *Build with agentic AI challenge*, this project analyzes crime risk and wellbeing data in Toronto's neighborhoods.  
-    It also enables the user to predict crime risk and using the data provide proactive steps for people to take. 
+    It also enables the user to predict crime risk.  Furthermore, it will leverage the predicted data and provide relevant information that people can take to stay safe. 
     It is built using [streamlit](https://streamlit.io/cloud) and [IBM Watson x](https://www.ibm.com/products/watsonx).  
     The data is from [Toronto Police](https://data.torontopolice.on.ca/datasets/TorontoPS::major-crime-indicators-open-data/about) and [City of Toronto](https://data.urbandatacentre.ca/organization/city-of-toronto-open-data?q=wellbeing&sort=score+desc%2C+metadata_modified+desc&page=1).
     """
