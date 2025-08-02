b82b1596e06b4ab2a82f0248e5e374a8ee1f6052
Edima <edimaudo@gmail.com>
2025-08-02 15:32:47 +0000

    Updated utils file

--- utils.py
+++ utils.py
@@ -9,3 +9,27 @@ import warnings
 import random
 #import plotly.express as px
 #import plotly.graph_objects as go
+
+"""
+Dashboard Information
+"""
+
+
+APP_NAME = 'TO Neighborhood Risk Predictor'
+ABOUT_HEADER = 'About'
+NEIGHBORHOOD_HEADER = "Neighborhood Risk Exploration"
+OVERVIEW_HEADER = 'Risk Overview'
+PREDICTON_HEADER = 'Neighborhood Risk Prediction'
+APP_FILTERS = 'Filters'
+NO_DATA_INFO = 'No data available to display based on the filters'
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+st.set_page_config(
+    page_title=APP_NAME,
+    page_icon="🌍",
+    layout="wide"
+)
+
+"""
+Load data
+"""
