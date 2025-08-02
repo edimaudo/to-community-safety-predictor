23315c31455981a4d7c4098cdf0371aa1d00b598
Edima <edimaudo@gmail.com>
2025-08-02 16:07:47 +0000

    updated utils

--- utils.py
+++ utils.py
@@ -38,3 +38,8 @@ st.set_page_config(
 Data
 """
 
+
+month_dict = {'January':1,'February':2,'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 
+'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
+
+dow_dict = {'Monday':1,'Tuesday':2,'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday':7}
