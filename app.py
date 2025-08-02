a1e28a63a7b9e16f27826ba83ff8c7d397773611
Edima <edimaudo@gmail.com>
2025-08-02 16:11:24 +0000

    updated app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from utils import *
 
 pg = st.navigation([
     st.Page("About.py"),
-    st.Page("Risk_Overview.py"),
+    st.Page("Overview.py"),
     st.Page("Neighborhood_Risk_Exploration.py"),
     st.Page("Neighborhood_Risk_Predictor.py"),
 ])
