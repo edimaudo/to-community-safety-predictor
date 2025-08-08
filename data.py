50a66fbc8672feadfb4b0c2cc8470e21242b8ee9
Edima <edimaudo@gmail.com>
2025-08-08 19:09:14 +0000

    updated data process

--- data.py
+++ data.py
@@ -8,52 +8,68 @@ Data
 """
 # Load data
 @st.cache_data
-def load_data(DATA_URL):
-    data = pd.read_excel(DATA_URL)
+def load_data(DATA_URL,DATA_TYPE):
+    if DATA_TYPE == 'xlsx':
+        data = pd.read_excel(DATA_URL)
+    elif DATA_TYPE == 'geojson':
+        data = pd.read_json(DATA_URL)
     return data
 
-def load_geojson(DATA_URL):
-    data = pd.read_json(DATA_URL)
-    return data
 
+# crime risk data
+df = load_data("data/Major_Crime_Indicators_Open_Data.xlsx",'xlsx')
 
+# wellbeing data
+wellbeing_culture = load_data("data/wellbeing-toronto-culture.xlsx",'xlsx')
+wellbeing_economics = load_data("data/wellbeing-toronto-economics.xlsx",'xlsx')
+wellbeing_environment = load_data("data/wellbeing-toronto-environment.xlsx",'xlsx')
+wellbeing_health = load_data("data/wellbeing-toronto-health.xlsx",'xlsx')
+wellbeing_transportation = load_data("data/wellbeing-toronto-transportation.xlsx",'xlsx')
+wellbeing_housing =  load_data("data/wellbeing-toronto-housing.xlsx",'xlsx')
+wellbeing_recreation =  load_data("data/wellbeing-toronto-recreation.xlsx",'xlsx')
 
-with st.spinner("loading data in progress", show_time=True):
-    # crime risk data
-    df = load_data("data/Major_Crime_Indicators_Open_Data.xlsx")
+# neighborhood
+neighborhood_improvement_area = load_data("data/neighbourhood-improvement-areas.xlsx",'xlsx')
+neighborhood = load_data("data/Neighbourhoods.xlsx","xlsx")
 
-    # wellbeing data
-    wellbeing_culture = load_data("data/wellbeing-toronto-culture.xlsx")
-    wellbeing_economics = load_data("data/wellbeing-toronto-economics.xlsx")
-    wellbeing_environment = load_data("data/wellbeing-toronto-environment.xlsx")
-    wellbeing_health = load_data("data/wellbeing-toronto-health.xlsx")
-    wellbeing_transportation = load_data("data/wellbeing-toronto-transportation.xlsx")
-    wellbeing_housing =  load_data("data/wellbeing-toronto-housing.xlsx")
-    wellbeing_recreation =  load_data("data/wellbeing-toronto-recreation.xlsx")
-
-    # neighborhood
-    neighborhood = load_geojson("data/Neighbourhoods.geojson") #gpd.read_file('data/Neighbourhoods.geojson')
-    neighborhood_improvement_area = load_data("data/neighbourhood-improvement-areas.xlsx")
 
 # Data Information
-YEAR =  df['OCC_YEAR'].unique()
-YEAR  = YEAR.astype('int')
+YEAR = (
+    df.loc[(df['OCC_YEAR'] >= 2014) & (df['OCC_YEAR'] <= 2025), 'OCC_YEAR']
+      .unique()
+)
+YEAR = YEAR.astype(int)
 YEAR.sort()
 
-MONTH = df['OCC_MONTH'].unique()
-MONTH  = MONTH.astype('str')
-MONTH = pd.DataFrame(MONTH,columns = ['Month'])
-month_dict = {'January':1,'February':2,'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 
-'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
-MONTH = MONTH.sort_values('Month', key = lambda x : x.apply (lambda x : month_dict[x]))
-MONTH = MONTH['Month'].values.tolist()
+month_dict = {
+    'January': 1, 'February': 2, 'March': 3, 'April': 4,
+    'May': 5, 'June': 6, 'July': 7, 'August': 8,
+    'September': 9, 'October': 10, 'November': 11, 'December': 12
+}
 
-DAY_OF_WEEK = df['OCC_DOW'].unique()
-DAY_OF_WEEK   = DAY_OF_WEEK.astype('str')
-DAY_OF_WEEK = pd.DataFrame(DAY_OF_WEEK,columns = ['DOW'])
-dow_dict = {'Monday':1,'Tuesday':2,'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday':7}
-DAY_OF_WEEK = DAY_OF_WEEK.sort_values('DOW', key = lambda x : x.apply (lambda x : dow_dict[x]))
-DAY_OF_WEEK = DAY_OF_WEEK['DOW'].values.tolist()
+MONTH = (
+    pd.DataFrame({'Month': df['OCC_MONTH'].dropna().unique()})  # remove NaN here
+    .assign(order=lambda d: d['Month'].str.strip().map(month_dict))
+    .dropna(subset=['order'])  # also removes anything not in month_dict
+    .sort_values('order')
+    ['Month'].tolist()
+)
+
+
+dow_dict = {
+    'Monday': 1, 'Tuesday': 2, 'Wednesday': 3,
+    'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7
+}
+
+DAY_OF_WEEK = (
+    pd.Series(df['OCC_DOW'].dropna().unique())
+      .str.strip()                      # remove extra spaces
+      .str.title()                      # normalize capitalization
+      .drop_duplicates()
+      .sort_values(key=lambda x: x.map(dow_dict))
+      .tolist()
+)
+
 
 HOUR=  df['OCC_HOUR'].unique()
 HOUR  = HOUR.astype('int')
@@ -74,3 +90,5 @@ NEIGHBORHOOD.sort()
 PREMISES_TYPE = df['PREMISES_TYPE'].unique()
 PREMISES_TYPE  = PREMISES_TYPE.astype('str')
 PREMISES_TYPE.sort()
+
+ 
