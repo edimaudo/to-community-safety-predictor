16be19e1ca79cc58fe7a9960f0a3e28512fac955
Edima <edimaudo@gmail.com>
2025-08-08 18:27:33 +0000

    update

---
+++ Overview.py
@@ -0,0 +1,14 @@
+from utils import * 
+from data import *
+
+st.title(APP_NAME)
+st.header(OVERVIEW_HEADER)
+
+with st.sidebar:
+    year_options = st.multiselect('Year',YEAR,default=YEAR)
+    month_options = st.multiselect('Month',MONTH,default=MONTH)
+    dow_options = st.multiselect('Day of Week',DAY_OF_WEEK,default=DAY_OF_WEEK)
+    mci_options = st.multiselect('Crime Type',MCI_CATEGORY,default=MCI_CATEGORY)
+    premises_options = st.multiselect('Premises Type',PREMISES_TYPE,default =PREMISES_TYPE)
+
+
