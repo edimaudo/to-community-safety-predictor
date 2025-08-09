5b420c3780bc0f5ad574f7e7f0aa82cdfa4a423f
Edima <edimaudo@gmail.com>
2025-08-09 15:30:40 +0000

    update

--- Neighborhood_Risk_Predictor.py
+++ Neighborhood_Risk_Predictor.py
@@ -1,4 +1,19 @@
+from utils import *
+from data import *
+from config import *
 
+st.title(APP_NAME)
+st.header(PREDICTON_HEADER)
 
+with st.sidebar:
+    month_options = st.selectbox('Month',MONTH)
+    dow_options = st.selectbox('Day of Week',DAY_OF_WEEK)
+    hour_options = st.selectbox('Hour',HOUR)
+    premises_options = st.selectbox('Premises Type',PREMISES_TYPE)
+    neighbourhood_options = st.selectbox('Neighborhood',NEIGHBORHOOD)
+    clicked = st.button("Run Prediction")
 
+st.subheader("Prediction")
+
+st.subheader("Community Risk Insights + Actions")
 ## Add LLM here
