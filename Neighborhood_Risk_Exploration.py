bfb676a1c19d670db22d337f0bcea55472550207
Edima <edimaudo@gmail.com>
2025-08-10 17:50:24 +0000

    ADDED HEADER

--- Neighborhood_Risk_Exploration.py
+++ Neighborhood_Risk_Exploration.py
@@ -1,3 +1,16 @@
+from utils import * 
+from data import *
+
+st.title(APP_NAME)
+st.header(NEIGHBORHOOD_HEADER)
+
+st.write("""
+         Highlights Socio-economic Metrics and Crime Risk trends for the selected neighborhood in Toronto.  
+         """)
+
+
+tab1, tab2 = st.tabs(["Socio-Ecomonic Metrics", "Crime Risk Trends"])
+
 
 
 # Add LLM Here
